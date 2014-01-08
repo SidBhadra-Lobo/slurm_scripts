5753ecb3c7545e98ee93c817bd8172cb3f2952a0
Sid Bhadra-Lobo <sbhadralobo@ucdavis.edu>
2014-01-07 18:16:20 -0800

    multi-lane test 1

--- CRM2_BWA_abun_test.sh
+++ CRM2_BWA_abun_test.sh
@@ -5,7 +5,7 @@
 ##########
 
 ## -D sets directory
-#SBATCH -D /home/sbhadral/Projects/
+#SBATCH -D /home/sbhadral/Projects/CRM2_abun
 
 ## -J sets the job name
 #SBATCH -J CRM2_abun
@@ -13,14 +13,26 @@
 ## -p sets the partition to run the job on
 #SBATCH -p hi
 
-## -o sets the destination for the stdout. %j includes the job number in the name.
-#SBATCH -o /home/sbhadral/Projects/slurm_log/CRM2_abun_stdout_%j.txt 
+## -o sets the destination for the stdout. %j sets the job name, %a includes the file name.
+#SBATCH -o /home/sbhadral/Projects/slurm_log/CRM2abun_%j_%a.out
 
 ## -e sets the destination for the stderr.
 #SBATCH -e /home/sbhadral/Projects/slurm_log/CRM2_abun_stderr_%j.txt
 
 ## -c sets number of cpus required per task
 #SBATCH -c 1
+
+list=$(*[1-2].txt.bz2)
+
+for %a in list
+
+do	
+
+## Run script.
+	srun /home/sbhadral/Projects/scripts/CRM2_BWA_abun.sh 
+	
+
+done
 
 ########## script starts here 
 ########## "!!" begins comments for things I'm not sure about.
@@ -39,13 +51,14 @@ bwa index -p CRM2_abun/UniqueCRM2 /home/sbhadral/Projects/CRM2_abun/UniqueCRM2.f
 ## NOTE: samtools is already available in your path, so no need to load it's module.
 ## samtools view (-S) specifies that the input is in .sam and (-b) sets the output format to .bam
 
-bwa mem CRM2_abun/UniqueCRM2 <(bzip2 -dc CRM2_abun/B73_FC42G13AAXX_1_1.txt.bz2)  <(bzip2 -dc CRM2_abun/B73_FC42G13AAXX_1_2.txt.bz2) | samtools view -Sb - > check.bam
+bwa mem CRM2_abun/UniqueCRM2 <(bzip2 -dc list)  <(bzip2 -dc list) | samtools view -Sb - > check.bam
 
 ## From the check.bam, run flagstat for alignment statistics.
 ## From the outputs, isolate the lines that show total reads per lane (sed -n -e1p) and total reads mapped per lane (-e 3p).
 ## Save these two numbers to a file (reads.stat).
 
 samtools flagstat /home/sbhadral/Projects/check.bam  | sed -n -e 1p -e 3p | cut -d " " -f 1 
+
 ## Remove excess files.
 rm check.bam
 
