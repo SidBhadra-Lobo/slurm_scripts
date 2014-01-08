8ede854b06da81f9477853848a1609e147452e49
Sid Bhadra-Lobo <sbhadralobo@ucdavis.edu>
2014-01-07 16:06:02 -0800

    efficiency trial 3

--- CRM2_BWA_abun_test.sh
+++ CRM2_BWA_abun_test.sh
@@ -39,16 +39,16 @@ bwa index -p CRM2_abun/UniqueCRM2 /home/sbhadral/Projects/CRM2_abun/UniqueCRM2.f
 ## NOTE: samtools is already available in your path, so no need to load it's module.
 ## samtools view (-S) specifies that the input is in .sam and (-b) sets the output format to .bam
 
-bwa mem CRM2_abun/UniqueCRM2 <(bzip2 -dc CRM2_abun/B73_FC42G13AAXX_1_1.txt.bz2)  <(bzip2 -dc CRM2_abun/B73_FC42G13AAXX_1_2.txt.bz2) | samtools view -Sb /dev/stdin /dev/stdout | samtools flagstat /dev/stdin | sed -n -e 1p -e 3p | cut -d " " -f 1 > reads.stat
+bwa mem CRM2_abun/UniqueCRM2 <(bzip2 -dc CRM2_abun/B73_FC42G13AAXX_1_1.txt.bz2)  <(bzip2 -dc CRM2_abun/B73_FC42G13AAXX_1_2.txt.bz2) | samtools view -Sb - > check.bam
 
 ## From the check.bam, run flagstat for alignment statistics.
 ## From the outputs, isolate the lines that show total reads per lane (sed -n -e1p) and total reads mapped per lane (-e 3p).
 ## Save these two numbers to a file (reads.stat).
 
-##samtools flagstat /home/sbhadral/Projects/check.bam  | sed -n -e 1p -e 3p | cut -d " " -f 1 > reads.stat
+samtools flagstat /home/sbhadral/Projects/check.bam  | sed -n -e 1p -e 3p | cut -d " " -f 1 | /dev/stdout
 
 ## Remove excess files.
-##rm check.bam
+rm check.bam
 
 ##########
 ##END
