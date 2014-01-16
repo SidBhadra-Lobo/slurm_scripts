166da79265b97508f0446cdce971ace06036e1ca
Sid Bhadra-Lobo <sbhadralobo@ucdavis.edu>
2014-01-16 12:02:41 -0800

    rework: success

--- CRM2_BWA_abun_test.sh
+++ CRM2_BWA_abun_test.sh
@@ -8,22 +8,19 @@
 #SBATCH -D /home/sbhadral/Projects/CRM2_abun/
 
 ## -J sets the job name
-##SBATCH -J CRM2_abun
+#SBATCH -J CRM2_abun
 
 ## -p sets the partition to run the job on
 #SBATCH -p serial
 
 ## -o sets the destination for the stdout. %j sets the job name.
-#SBATCH -o /home/sbhadral/Projects/slurm_log/CRM2_out_%j.txt
+#SBATCH -o /home/sbhadral/Projects/slurm_log/%j.out
 
 ## -e sets the destination for the stderr.
-#SBATCH -e /home/sbhadral/Projects/slurm_log/CRM2_stderr_%j.txt
+#SBATCH -e /home/sbhadral/Projects/slurm_log/%j.err
 
 ## -c sets number of cpus required per task
-#SBATCH -c 1
-
-## -N requests the min and/or max number of nodes to be allocated to this job.
-#SBATCH -N 3
+#SBATCH -c 8
 
 ## -e (errexit) Exit immediately if a simple command exits with a non-zero status
 set -e
@@ -36,34 +33,47 @@ set -u
 #SBATCH --mail-user=sbhadralobo@ucdavis.edu 
 
 
-## Loop through all files while executing the script across all files in parallel.
-
 #### command line input for running this script.
 
 ## cd /home/sbhadral/Projects/CRM2_abun
 
 #### Load the BWA module. Once the module is loaded, it is already in your path. So just 'bwa mem' will suffice.
 
-## for file in "$(ls *[1-2].txt.bz2)" ; do module load /share/apps/modulefiles/hpc/bwa/0.7.5a ; sbatch /home/sbhadral/Projects/scripts/CRM2_BWA_abun_test.sh "$file" ; done
+##module load /share/apps/modulefiles/hpc/bwa/0.7.5a
 
+## Indexing the reference TE.
+
+##bwa index -p UniqueCRM2 UniqueCRM2.fasta
+
+## Loop through all files while executing the script across all files in parallel.
+
+## for file in $(ls *1.txt.bz2) ; do sbatch /home/sbhadral/Projects/scripts/CRM2_BWA_abun_test.sh $file ; done
+
+module load bwa/0.7.5a
 	
 file1=$1
 file2=$(echo $file1 | sed -e 's/_1_1/_1_2/g')
 file3=$(echo $file1 | sed -e 's/_[1-2]\.txt.bz2//')
-	
+
+# for file in *%j.out; 
+# 		
+# 		do
+#     		sed -e "s/\$/ $file/" "$file"
+# 
+# done		
 	
 #### Since this is a test run, the files of interest will already be in my directory and not in ~/group/
 
 
 ## Indexing the reference TE.
 
-bwa index -p UniqueCRM2 UniqueCRM2.fasta
+#bwa index -p UniqueCRM2 UniqueCRM2.fasta
 
 ## Take a test paired end file and run BWA mem, convert .sam to .bam, isolate flagstat output lines that show total reads per lane (sed -n -e1p) and total reads mapped per lane (-e 3p). Send to stdout.
 ## NOTE: samtools is already available in your path, so no need to load it's module.
 ## samtools view (-S) specifies that the input is in .sam and (-b) sets the output format to .bam
 
-bwa mem UniqueCRM2 <(bzip2 -dc $file1 )  <(bzip2 -dc $file2 ) | samtools view -Sb - > check.bam
+bwa mem UniqueCRM2 <(bzip2 -dc $file1 )  <(bzip2 -dc $file2 ) | samtools view -Sb - > check.%j.bam
 
 ## From the check.bam, run Samtools flagstat for alignment statistics, pipe to stdout.
 ## From the outputs, isolate the lines that show total reads per lane (sed -n -e1p) and total reads mapped per lane (-e 3p).
@@ -73,17 +83,21 @@ bwa mem UniqueCRM2 <(bzip2 -dc $file1 )  <(bzip2 -dc $file2 ) | samtools view -S
 ## Concatenate all lane alignment outputs into a single table.
 ## Save to a single file in Projects/
 
-samtools flagstat check.bam 	| 
+
+echo $file3 $( samtools flagstat check.%j.bam 	| 
 		sed -n -e 1p -e 3p 		| 
 			cut -d " " -f 1 	| 
-					paste -d ' ' - - > "${file3}".reads 	;
+					paste -d ' ' - -  )					
 
-							echo "${file3}"  	| 
-									paste -d ' ' - "${file3}".reads > /home/sbhadral/Projects/"${file3}".abun
+							#echo $file3  	| 
+							#		paste -d ' ' - - > /home/sbhadral/Projects/slurm_log/$file3.name
+							
 
+		
+			
 ## Remove excess files.
-rm check.bam
-rm "${file3}".reads
+rm check.%j.bam
+
 
 
 
